0d0ce41c7c6594cbad745333cc5e132ab8f4b183
robertomc20 <roberto.marihuan.crisostomo@gmail.com>
2021-05-27 09:54:07 -0400

    first commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+
+    // const categories = ['One Punch', 'Dragon Ball', 'Kimetsu No Yaiba'];
+    const [categories, setCategories] = useState(['One Punch']);
+
+    /*
+    const handleAdd = () => {
+        // setCategories( [...categories, 'Naruto'] );
+        setCategories( cats => [...cats, 'Naruto'] );
+        
+    }
+    */
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+
+            <AddCategory setCategories={ setCategories } />
+
+            <hr/>
+
+            
+
+            <ol>
+                {
+                    categories.map( ( category ) =>  
+                        <GifGrid 
+                            key={ category }
+                            category={ category } 
+                        />
+                    )
+                }
+            </ol>
+
+        </>
+    );
+
+}
+
+
---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+import { getGifs } from '../helpers/getGifs'
+
+// custom hooks
+export const useFetchGifs = ( category ) => {
+
+    const [state, setState] = useState({
+        data: [],
+        loading: true
+    });
+
+
+    useEffect( () => {
+        
+        getGifs( category )
+            .then( imgs => {
+
+                setState({
+                    data: imgs,
+                    loading: false
+                });
+                
+            });
+
+    }, [ category ]);
+
+
+    return state; // { data:[], loading: true}
+
+
+}
